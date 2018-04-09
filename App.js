a79dc07a6a0dc7e2bcdab26d6951c8ae060e8405
Tran Dinh Quang (01642499284) <quang500040@gmail.com>
2018-04-09 17:11:29 +0700

    Init Code

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { StatusBar, View } from "react-native";
+import MainNavigation from "./src/routes/MainNavigation";
+
+export default class App extends React.Component {
+  render() {
+    return <TabBar />;
+  }
+}
---
+++ ios/Pods/Target Support Files/Pods-easymoney/Pods-easymoney-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_easymoney : NSObject
+@end
+@implementation PodsDummy_Pods_easymoney
+@end
